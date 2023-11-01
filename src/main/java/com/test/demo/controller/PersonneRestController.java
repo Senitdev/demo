7fa4ab63bc894e04d3585e8d8815e8c354fe1a2c
Senitdev <pape.toure@senitdev.com>
2023-11-01 23:18:31 +0000

    Class model Personne
    Refactory class Personne

--- src/main/java/com/test/demo/controller/PersonneRestController.java
+++ src/main/java/com/test/demo/controller/PersonneRestController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.test.demo.service.PersonneService;
-
+import com.test.demo.model.Personne;
 import lombok.AllArgsConstructor;
 
 @RestController
--- src/main/java/com/test/demo/repository/PersonneRepository.java
+++ src/main/java/com/test/demo/repository/PersonneRepository.java
@@ -1,6 +1,6 @@
 package com.test.demo.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
-import com.test.demo.Personne;
+import com.test.demo.model.Personne;
 public interface PersonneRepository extends JpaRepository<Personne,Long> {
     
 }
